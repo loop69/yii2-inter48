ca46ea3627dc4eaf50509b2da9d37437489f72f7
loop69 <omusatov@gmail.com>
2016-01-31 21:07:44 +0300

    user profile begining

---
+++ modules/user/controllers/ProfileController.php
@@ -0,0 +1,40 @@
+<?php
+namespace app\modules\user\controllers;
+
+use app\modules\user\models\User;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use Yii;
+
+class ProfileController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index', [
+            'model' => $this->findModel(),
+        ]);
+    }
+
+    /**
+     * @return User the loaded model
+     */
+    private function findModel()
+    {
+        return User::findOne(Yii::$app->user->identity->getId());
+    }
+}
